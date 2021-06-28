d3fdc05d2cbae1a6f6cc0905bbc851e0a7b6b11b
TomasPorcaro <tomasporcaro22@gmail.com>
2021-06-28 18:56:05 -0300

    init repo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter basename="./" >
+                
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
